389eaa666f97e77209e6b130657bbc27faf3be70
Ikasuu <50899452+freemanxjl@users.noreply.github.com>
2024-03-13 18:11:51 -0700

    Replaced username with bucketUrl

--- services/advertising-service/main.py
+++ services/advertising-service/main.py
@@ -24,9 +24,6 @@ def create_bucket_if_not_exists(bucket_name):
 def upload_photo():
     # Get username from request parameters
     username = request.args.get('username')
-    bucketUrl = request.bucket.get('bucket')
-    if not bucketUrl:
-        return jsonify({'error': 'Bucket URL parameter is required'}), 400
 
     if not username:
         return jsonify({'error': 'Username parameter is required'}), 400
@@ -37,7 +34,7 @@ def upload_photo():
         return jsonify({'error': 'File parameter is required'}), 400
 
     # Upload file to Google Cloud Storage
-    bucket_name = f'{bucketUrl}'
+    bucket_name = f'{username}'
     bucket = create_bucket_if_not_exists(bucket_name)
     blob = bucket.blob(file.filename)
     blob.upload_from_file(file)
@@ -48,9 +45,6 @@ def upload_photo():
 def download_photo():
     # Get username from request parameters
     username = request.args.get('username')
-    bucketUrl = request.bucket.get('bucket')
-    if not bucketUrl:
-        return jsonify({'error': 'Bucket URL parameter is required'}), 400
     if not username:
         return jsonify({'error': 'Username parameter is required'}), 400
 
@@ -60,7 +54,7 @@ def download_photo():
         return jsonify({'error': 'Filename parameter is required'}), 400
 
     # Download file from Google Cloud Storage
-    bucket_name = f'{username}-bucket'
+    bucket_name = f'{username}'
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(filename)
     file_path = f'/tmp/{filename}'  # Save the file to /tmp directory
