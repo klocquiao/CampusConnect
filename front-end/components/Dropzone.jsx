c80a76aa3e613785b387aaabd4a63d3b651f4d5c
Ikasuu <50899452+freemanxjl@users.noreply.github.com>
2024-03-13 18:21:30 -0700

    Update Dropzone.jsx

--- front-end/components/Dropzone.jsx
+++ front-end/components/Dropzone.jsx
@@ -1,15 +1,23 @@
-import React, {useCallback} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
 import { uploadImage } from '../apis/apis';
 
 const Dropzone = (props) => {
+    const [bucketUrl, setBucketUrl] = useState("");
+
+    useEffect(() => {
+        if(props.bucketUrl !== ""){
+            setBucketUrl(props.bucketUrl);
+            console.log(props.bucketUrl);
+        }
+    }, [props.bucketUrl]);
     
     const onDrop = useCallback(acceptedFiles => {
             const formData = new FormData();
             const file = acceptedFiles.at(-1);
             const username = 'swag'.concat('_user');
-            const bucketUrl = props.bucketUrl;
             formData.append('file', file);
+            console.log("TEST" + " " + bucketUrl);
             uploadImage(bucketUrl, formData, file, username); // TODO change user name to actual username
             props.setRecentImage(file);
             props.setUploadStatus(respJson.message);
